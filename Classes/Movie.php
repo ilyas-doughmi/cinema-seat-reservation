eb7942b4352ecfbb96b3acc68d31f0c1d134535c
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-09 23:01:53 +0100

    feat: adding new function to getmovie data using his id

--- Classes/Movie.php
+++ Classes/Movie.php
@@ -7,4 +7,13 @@ Class Movie extends db{
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
+    public function getMovieData($id){
+        $query = "SELECT * FROM movie WHERE movie_id = :id";
+        $stmt = $this->connect()->prepare($query);
+        $stmt->bindParam(":id",$id);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
 }
