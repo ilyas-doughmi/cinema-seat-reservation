e94d42b24081d5e5cfdb22812b63902dfa89b6a4
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-09 19:43:27 +0100

    feat: showing all images with there names

--- index.php
+++ index.php
@@ -7,3 +7,18 @@ $movies = new Movie();
 $available_movies = $movies->getAllMovies();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($available_movies as $mv){?>
+        <h1><?= $mv["movie_name"] ?></h1>
+        <img src="<?= $mv["movie_image"] ?>" alt="">
+  <?php  } ?>
+</body>
+</html>
